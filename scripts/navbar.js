ea1c5ef022fede34d0ab179586d240c3906d7f66
Ansskhan <Anas771khan@hotmail.com>
2025-08-15 21:42:43 +0500

    foot-links fixed

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -105,12 +105,12 @@ footer.innerHTML = `
     <div class="footer-links" >
       <h3>Quick Links</h3>
        <ul>
-        <li><a href="../index.html" ><i  class="fas fa-home"></i> Home</a></li>
-        <li><a href="../destinations.html"><i class="fas fa-map-marked-alt"></i> Destinations</a></li>
-        <li><a href="../tours.html"><i class="fas fa-suitcase"></i> Tours</a></li>
-        <li><a href="../about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
-        <li><a href="../contact.html"><i class="fas fa-phone-alt"></i> Contact</a></li>
-        <li><a href="../sitemap.html"><i class="fas fa-phone-alt"></i> Sitemap</a></li>
+        <li><a href="index.html" ><i  class="fas fa-home"></i> Home</a></li>
+        <li><a href="destinations.html"><i class="fas fa-map-marked-alt"></i> Destinations</a></li>
+        <li><a href="tours.html"><i class="fas fa-suitcase"></i> Tours</a></li>
+        <li><a href="about.html"><i class="fas fa-info-circle"></i> About Us</a></li>
+        <li><a href="contact.html"><i class="fas fa-phone-alt"></i> Contact</a></li>
+        <li><a href="sitemap.html"><i class="fas fa-phone-alt"></i> Sitemap</a></li>
       </ul>
     </div>
 
